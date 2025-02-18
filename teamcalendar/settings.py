b79b23a7d15cc1866b224b0b33a05aec9f025c2f
Aleksandra Balcerzak <aleksbalcerzakdev@gmail.com>
2025-02-18 01:11:29 +0100

    Added requirements.txt with Django

--- teamcalendar/settings.py
+++ teamcalendar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-vsfacmeeg6nfk!)%yp%q#)9^%2a&j@1y2-fna)r2)p2d!hc7xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["meetappdate-dmc3fkgthxdsapd7.polandcentral-01.azurewebsites.net"]
 
 
 # Application definition
@@ -116,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
