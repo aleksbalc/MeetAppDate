036e078d6ed7526053217b567fb683c58caafc88
Aleksandra Balcerzak <aleksbalcerzakdev@gmail.com>
2025-02-18 05:38:03 +0100

    Fixed secret key

--- teamcalendar/settings.py
+++ teamcalendar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DB_NAME', 'meetappdate_db')
+SECRET_KEY = os.getenv('APP_SECRET_KEY', 'meetappdatedatabase')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
