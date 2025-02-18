ec2db0e8c9d6092bc042b28cbbc85e68b166e5b7
Aleksandra Balcerzak <aleksbalcerzakdev@gmail.com>
2025-02-18 06:07:21 +0100

    Debugging variables

--- teamcalendar/settings.py
+++ teamcalendar/settings.py
@@ -31,7 +31,7 @@ print("DB_NAME:", os.getenv("DB_NAME"))
 print("DB_USER:", os.getenv("DB_USER"))
 print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
 print("DB_HOST:", os.getenv("DB_HOST"))
-print("DB_PORT:", os.getenv("DB_PORT"))
+print("APP_SECRET_KEY:", os.getenv("APP_SECRET_KEY"))
 
 
 # Application definition
