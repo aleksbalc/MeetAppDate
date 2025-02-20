4ca47c62694ed3d5dca433ea0b41c59666c6eedf
Aleksandra Balcerzak <aleksbalcerzakdev@gmail.com>
2025-02-20 14:41:25 +0100

    debugging

--- teamcalendar/settings.py
+++ teamcalendar/settings.py
@@ -167,8 +167,8 @@ DATE_FORMAT = '%Y-%m-%d'  # Default display format
 
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com' )
 EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", True)
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False)
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 
